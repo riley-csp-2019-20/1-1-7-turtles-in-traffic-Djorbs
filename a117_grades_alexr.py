2b58778c93aa802703819e4b38ea94777d8ae0e8
Djorbs <55500961+Djorbs@users.noreply.github.com>
2019-09-27 11:33:57 -0400

    I have no idea

---
+++ a117_grades_alexr.py
@@ -0,0 +1,27 @@
+#   a117_grades.py
+#   This code is incomplete.
+my_courses = ["English", "Math", "CS"]
+course = 0
+
+check_grades = True
+while (check_grades):
+    print() # blank line
+print("Enter your points for " + course)
+
+a = int(input("Points -> "))
+redo = input("Do you need to re-do these grades? (y/n)")
+if (redo == "y"):
+	check_grades = True
+else:
+	check_grades = False
+for course in my_courses:
+    if (a >= 90):
+	    print("A")
+    elif (a >= 80):
+	    print("B")
+    elif (a >= 70):
+	    print("C")
+    elif (a >= 60):
+	    print("D")
+    else:
+	    print("F")
